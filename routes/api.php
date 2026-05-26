09e2c08b5f86dec504cd9597c513921063d4e352
Arthur M <arthurmccomp@gmail.com>
2026-05-26 00:51:26 +0000

    new file:   app/Http/Controllers/ItemPedidoController.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,19 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PedidosController;
 use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\ItemPedidoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function () {  
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
     Route::prefix('produtos')->group(function () {
         Route::get('/', [ProdutosController::class, 'index']);
@@ -29,9 +28,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('pedidos')->group(function () {
         Route::get('/', [PedidosController::class, 'index']);
         Route::post('/', [PedidosController::class, 'store']);
-        Route::get('/{id}', [PedidosController::class, 'show'])->whereNumber('id');
-        Route::put('/{id}', [PedidosController::class, 'update'])->middleware('pedido.owner')->whereNumber('id');
-        Route::delete('/{id}', [PedidosController::class, 'destroy'])->middleware('pedido.owner')->whereNumber('id');
+        Route::get('/{id}', [PedidosController::class, 'show']);
+        Route::put('/{id}', [PedidosController::class, 'update']);
+        Route::delete('/{id}', [PedidosController::class, 'destroy']);
+    });
+
+    Route::prefix('itens-pedido')->group(function () {
+        Route::get('/', [ItemPedidoController::class, 'index']);
+        Route::post('/', [ItemPedidoController::class, 'store']);
+        Route::get('/{id}', [ItemPedidoController::class, 'show']);
+        Route::put('/{id}', [ItemPedidoController::class, 'update']);
+        Route::delete('/{id}', [ItemPedidoController::class, 'destroy']);
     });
 
 });
